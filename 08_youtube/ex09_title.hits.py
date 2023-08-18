c8c2616a37391e9fc31264617f50d552c50e3f75
jscheo <981625@naver.com>
2023-08-18 16:20:03 +0900

    08-08 오후수업

--- 08_youtube/ex09_title.hits.py
+++ 08_youtube/ex09_title.hits.py
@@ -11,7 +11,7 @@ time.sleep(2)
 
 titles = driver.find_elements(By.XPATH, '//*[@id="video-title"]')
 for title in titles:
-    if title.get_attribute("aria-label"): # shorts 영상을 걸러내기 위한 조건문
+    if title.get_attribute("aria-label") and title.text : # shorts 영상을 걸러내기 위한 조건문
         # aria-label 속성값 가져오기
         aria_label = title.get_attribute("aria-label")
         print(aria_label)
